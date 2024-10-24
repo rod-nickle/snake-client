694e540d40435882a96bb22803e6e43646859b12
rod-nickle <rod@bitwise-it.com>
2024-10-23 19:50:12 -0600

    Moved connect function from play.js into client.js

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // log data from the server to the console
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,23 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost', //'172:28:217:226',
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // log data from the server to the console
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-  
-  return conn;
-};
-
+const connect = require("./client");
 
 console.log("Connecting ...");
 connect();
