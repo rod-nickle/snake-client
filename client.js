7a8b1bff02b9e2ce519e609ca6f0c96e46562c18
rod-nickle <rod@bitwise-it.com>
2024-10-23 20:02:40 -0600

    Modified client.js. Handle the "connect" event

--- client.js
+++ client.js
@@ -15,6 +15,14 @@ const connect = function() {
     console.log("Server says: ", data);
   });
 
+  // on connection to the server:
+  // - write a message to our console
+  // - send the server our username
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: Rod");
+  });
+
   return conn;
 };
 
